c6e1177efd98ed71de75315d160db93318389932
NAVENKUMAR D <naven.duraisamy5859@gmail.com>
2024-05-01 12:35:12 +0530

    initial setup

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+import json
+
+# Create a Flask app
+app = Flask(__name__)
+
+# Define a route and a view function
+@app.route('/')
+def hello_world():
+    return 'Hello, World! This is a simple Flask server.'
+
+@app.route('/',methods=['POST'])
+def foo():
+   data = json.loads(request.data)
+   print(data)
+   return "OK"
+
+
+if __name__ == '__main__':
+    # Run the app
+    app.run(debug=True)
